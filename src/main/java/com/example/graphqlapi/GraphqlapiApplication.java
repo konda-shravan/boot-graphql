1eff498fef519f74b2d5c250def553f41ec69712
konda shravan <konda.shravan@outlook.com>
2025-11-05 12:56:46 +0530

    Changed system info

--- src/main/java/com/example/graphqlapi/GraphqlapiApplication.java
+++ src/main/java/com/example/graphqlapi/GraphqlapiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GraphqlapiApplication {
 
 	public static void main(String[] args) {
+        System.out.println("GraphQL Demo");
 		SpringApplication.run(GraphqlapiApplication.class, args);
 	}
 
